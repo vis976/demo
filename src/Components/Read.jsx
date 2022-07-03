e920765bd66d889a91f3f5dfba11590f8a77c877
vis976 <vishalchaubey9628@gmail.com>
2022-07-03 17:47:17 +0530

    demo

---
+++ src/Components/Read.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "./Read.module.css"
+const Read = () => {
+  return (
+    <div>
+        <h1 style={{color:"red"}}>Hello we start Learning React Js </h1>
+        <h1 className={styles.container}>Together on our state....</h1>
+    </div>
+  )
+}
+
+export default Read
---
+++ src/Components/Vishal.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Read from './Read'
+
+const vishal = () => {
+  return (
+    <div>
+       <Read />
+       <div>
+        <h3>Thanks & Regards</h3>
+        <p>Vishal & Devashish</p>
+        </div>
+    </div>
+  )
+}
+
+export default vishal
